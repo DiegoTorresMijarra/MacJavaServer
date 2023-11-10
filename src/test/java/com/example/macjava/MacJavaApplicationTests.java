6b3ee1d31983b87514baa1d5742d964c016848fb
DiegoTorresMijarra <diegotorresmijarra@gmail.com>
2023-11-10 17:02:53 +0100

    Init Project

---
+++ src/test/java/com/example/macjava/MacJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.macjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
