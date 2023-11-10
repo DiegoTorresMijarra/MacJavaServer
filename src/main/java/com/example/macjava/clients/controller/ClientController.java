0b1de7d7ac867e6264206e38e08afb905144871e
jaime9lozano <jaime9lozano@gmail.com>
2023-11-10 22:00:41 +0100

    V 0.1

--- src/main/java/com/example/macjava/clients/controller/ClientController.java
+++ src/main/java/com/example/macjava/clients/controller/ClientController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.*;
 
 @RestController
