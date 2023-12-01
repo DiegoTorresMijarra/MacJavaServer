c57fddbe8eaca3a5a1a02cd22dbd2aceeac5b350
DiegoTorresMijarra <diegotorresmijarra@gmail.com>
2023-12-01 11:48:40 +0100

    feat(models)

---
+++ src/main/java/com/example/macjava/rest/orders/models/OrderedProduct.java
@@ -0,0 +1,48 @@
+package com.example.macjava.rest.orders.models;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderedProduct {
+    @Min(value = 1, message = "La cantidad del producto no puede ser negativa")
+    @Builder.Default
+    private Integer quantity = 1;
+    @Min(value = 1, message = "El id del producto debe ser mayor de 1")
+    private Long productId;
+    @Positive(message = "El precio del producto no puede ser negativo")
+    @Builder.Default
+    private double  productPrice= 1.0;
+    @Builder.Default
+    private double totalPrice = 1.0;
+
+    /**
+     * Establece la cantidad del producto y actualiza el precio total del pedido
+     * @param quantity nueva cantidad
+     */
+    public void setQuantity(@Min(value = 0) Integer quantity) {
+        this.quantity = quantity;
+        this.calculateTotalPrice();
+    }
+
+    /**
+     * Establece el precio del producto y actualiza el total
+     * @param productPrice product price
+     */
+    public void setProductPrice(@Positive Double productPrice){
+        this.productPrice = productPrice;
+        this.calculateTotalPrice();
+    }
+    public void calculateTotalPrice (){
+        setTotalPrice(productPrice * quantity);
+    }
+}
